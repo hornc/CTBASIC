85726df0dcbd76426b245f6d179f345268aac043
Chales Horn <charles.horn@gmail.com>
2022-12-08 11:33:11 +1300

    refactor CLS escape sequences

--- CTBASIC/graphics.py
+++ CTBASIC/graphics.py
@@ -11,13 +11,13 @@ GS = chr(29)   # Sets terminal to Graph Mode.
 US = chr(31)   # Resets terminal from Graph to Alpha Mode.
 
 # Character and control codes used for clearing screen in various terminal modes:
-CAN = chr(24)  # ASCII cancel, used as a NOP character in the combined ANSI control / Tek4010 CLS
-ANSI_HOME = '[H'
-ANSI_CLEAR = '[J'
+CAN = chr(24)  # ASCII cancel, used as a NOP padding character in the combined ANSI control / Tek4010 CLS
+ANSI_HOME = f'{ESC}[H'
+ANSI_CLEAR = f'{ESC}[J'
+RIS = f'{ESC}c'  # ANSI control Reset to Initial State (does not work on TEK4010)
 CLS_TEK = f'{ESC}{FF}'  # Tektronix 4010 clear screen
-CLS_ANSI1 = f'{ESC}{ANSI_HOME}{ESC}{ANSI_CLEAR}'  # ANSI control clear 1
-CLS_ANSI2 = f'{ESC}c'  # ANSI control clear 2
-CLS_BOTH = f'{ESC}c{ESC}{FF}{CAN}'
+CLS_ANSI = f'{ANSI_HOME}{ANSI_CLEAR}'  # ANSI control clear screen
+CLS_BOTH = f'{RIS}{ESC}{FF}{CAN}'
 CLS = CLS_BOTH
 
 
